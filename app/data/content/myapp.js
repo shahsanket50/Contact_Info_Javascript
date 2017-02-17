f1ca69dcbdac7b05abdbfb49298acacc3b5b8096
shahsanket50 <shahsanket50@gmail.com>
2017-02-17 17:18:54 +0530

    Added Validations

--- app/data/content/myapp.js
+++ app/data/content/myapp.js
@@ -1,6 +1,6 @@
 var app = angular.module('form', ['ngMaterial', 'ngMessages']).config(['$mdIconProvider', function ($mdIconProvider) {
     $mdIconProvider.icon('md-close', 'icons/ic_close_24px.svg', 24);
-      }]).controller('Ctrl',PrimaryCtrl);
+      }]).controller('Ctrl', PrimaryCtrl);
 
 function PrimaryCtrl($scope) {
     $scope.user = {
@@ -13,7 +13,7 @@ function PrimaryCtrl($scope) {
     $scope.facsimileTelephone = [];
     $scope.email = [];
     $scope.readonly = false;
-    var tabs = [
+    var addressTabs = [
             {
                 title: 'Enter Address'
                 , type: ''
@@ -27,22 +27,16 @@ function PrimaryCtrl($scope) {
         ]
         , selected = null
         , previous = null;
-    $scope.addressTypes = [
-        'Mailing', 'Physical', ' Mailing & Physical'
-    ];
-    $scope.tabs = tabs;
+    $scope.addressTabs = addressTabs;
     $scope.$watch('selectedIndex', function (current, old) {
         previous = selected;
-        selected = tabs[current];
-        if (old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
-        if (current + 1) $log.debug('Hello ' + selected.title + '!');
+        selected = addressTabs[current];
     });
     $scope.addTab = function () {
-        title = 'Address' + (tabs.length).toString();
-        console.log(title);
-        tabs[0].title = title;
-        tabs[0].visible = false;
-        tabs.unshift({
+        title = 'Address' + (addressTabs.length).toString();
+        addressTabs[0].title = title;
+        addressTabs[0].visible = false;
+        addressTabs.unshift({
             title: 'Enter Address'
             , type: ''
             , street: ''
@@ -52,34 +46,39 @@ function PrimaryCtrl($scope) {
             , pincode: ''
             , visible: true
         });
-};
+    };
     $scope.removeTab = function (tab) {
-        var index = tabs.indexOf(tab);
-        tabs.splice(index, 1);
+        var index = addressTabs.indexOf(tab);
+        addressTabs.splice(index, 1);
     };
-    $scope.selectOption = function (tab, option) {
-        tab.type = option;
+    $scope.validateEmail = function () {
+        var i = $scope.email.length;
+            var emailValue = $scope.email[i-1];
+        var reg = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+        if (reg.test(emailValue)) {
+            $scope.isEmailValid = true;
+        }
+        else {
+            $scope.isEmailValid = false;
+            $scope.email.pop();
+        }
     };
-    
-    $scope.validateEmail = function() {
-      var emailValue = angular.element(".md-input").val();
-    var reg = "/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/";
-    if (reg.test(emailValue)) {
-    $scope.isEmailValid = true;
-    } else {
-        $scope.isEmailValid = false;
-    }
- 
-  };
-    
-    $scope.validateTelephone = function() {
-      var telephoneValue = angular.element(".md-input").val();
-    var reg = "/^\d{10}$/";
-    if (reg.test(telephoneValueValue)) {
-    $scope.isTelephoneValid = true;
-    } else {
-        $scope.isTelephoneValid = false;
-    }
- 
-  };
+    $scope.validateTelephone = function () {
+            var i = $scope.voiceTelephone.length;
+            var telephoneValue = $scope.voiceTelephone[i-1];
+            var reg = /^\d{10}$/;
+            if (reg.test(telephoneValue)) {
+                $scope.isTelephoneValid = true;
+            }
+            else {
+                $scope.isTelephoneValid = false;
+                $scope.voiceTelephone.pop();
+            }
+//        }
+    };
+    $scope.submitForm = function () {
+        if ($scope.voiceTelephone.length != 0) {
+            
+        }
+    };
 }
